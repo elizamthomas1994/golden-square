ec2ecdd3efcbb3d8a34c4ee2251a895801f6c43b
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-09 18:59:41 +0000

    Modified grammar_checker and relevant spec files to include more tests

--- test_drive/lib/grammar_checker.rb
+++ test_drive/lib/grammar_checker.rb
@@ -1,3 +1,10 @@
 def grammar_checker(text)
-  text.empty? fail "Not a sentence."
+  fail "Not a sentence." if text.empty?
+  first_letter_checker = text[0] == text[0].upcase
+  last_letter_checker = [".", "?", "!"].include? text[-1]
+  if first_letter_checker && last_letter_checker
+    return true
+  else
+    return false
+  end
 end
