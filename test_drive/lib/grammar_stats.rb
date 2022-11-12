91fa71fc0321bb9358df65a9dd804cde00184556
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-12 21:58:26 +0000

    Added more tests to grammar_stats_spec file

--- test_drive/lib/grammar_stats.rb
+++ test_drive/lib/grammar_stats.rb
@@ -6,6 +6,14 @@ class GrammarStats
     def check(text) # text is a string
       # Returns true or false depending on whether the text begins with a capital
       # letter and ends with a sentence-ending punctuation mark.
+      fail "not a string" if text.empty?
+      first_letter_checker = text[0] == text[0].upcase
+      last_letter_checker = [".", "?", "!"].include? text[-1]
+      if first_letter_checker && last_letter_checker
+        return true
+      else
+        return false
+      end
     end
   
     def percentage_good
--- test_drive/spec/grammar_stats_spec.rb
+++ test_drive/spec/grammar_stats_spec.rb
@@ -4,4 +4,55 @@ RSpec.describe GrammarStats do
   it 'constructs' do
     grammar_stats = GrammarStats.new
   end
+
+  describe '#check' do
+    context 'given a string with capital and ending with full stop' do
+      it 'returns true' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("Hello world.")).to eq true
+      end
+    end
+
+    context 'given a string with capital and ending with exclamation mark' do
+      it 'returns true' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("How are you?")).to eq true
+      end
+    end
+
+    context 'given a string with capital and ending with question mark' do
+      it 'returns true' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("How are you?")).to eq true
+      end
+    end
+
+    context 'given a string with no capital nor punctuation' do
+      it 'returns false' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("hello world")).to eq false
+      end
+    end
+
+    context 'given a string with no capital and relevant punctuation' do
+      it 'returns false' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("hello world.")).to eq false
+      end
+    end
+
+    context 'given a string with capital and no punctuation' do
+      it 'returns false' do
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("Hello world")).to eq false
+      end
+    end
+
+    context 'given an empty string' do
+      it 'fails' do
+        grammar_stats = GrammarStats.new
+        expect{ grammar_stats.check("") }.to raise_error "not a string"
+      end
+    end
+  end
 end
