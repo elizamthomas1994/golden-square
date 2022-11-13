9e257b58da95b2d16e036868c13831ac0ae54296
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-13 16:01:00 +0000

    Created todo_tracker.rb, spec file and design file

---
+++ test_drive/lib/todo_tracker.rb
@@ -0,0 +1,19 @@
+class TodoTracker
+  def initialize
+  end
+  
+  def add(task) # task is a string representing todo item
+    # adds task to todo list
+    # returns nothing
+  end
+  
+  def list
+    # returns full list of tasks to be done
+  end
+  
+  def complete(task) # task is a string representing a complete task
+    # Removes completed tasks from the todo list
+    # Returns nothing
+    # Fails if the task to be completed does not exist
+  end
+end
