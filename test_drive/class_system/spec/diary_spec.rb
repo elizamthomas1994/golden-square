a57c13bb26125ec60dbba465705796fc56536409
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-14 21:31:41 +0000

    Added further tests to Diary and Diary Entry class system

--- test_drive/class_system/spec/diary_spec.rb
+++ test_drive/class_system/spec/diary_spec.rb
@@ -6,5 +6,20 @@ RSpec.describe Diary do
       diary = Diary.new
       expect(diary.all).to eq []
     end
+
+    it 'has a word count of 0' do
+      diary = Diary.new
+      expect(diary.count_words).to eq 0
+    end
+
+    it 'has a reading time of 0' do
+      diary = Diary.new
+      expect(diary.reading_time(2)).to eq 0
+    end
+
+    it 'has a best readble entry of nil' do
+      diary = Diary.new
+      expect(diary.find_best_entry_for_reading_time(2, 1)).to eq nil
+    end
   end
 end
