5ea7acdf6dfdd300fe86227937a75ee0fa583c70
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-17 20:55:41 +0000

    Created string_repeater.rb and spec file

---
+++ mocking_bite_challenges/Terminal_IO/spec/string_repeater_spec.rb
@@ -0,0 +1,18 @@
+require 'string_repeater'
+
+RSpec.describe StringRepeater do
+  it 'returns a string a given number of times using the terminal' do
+    terminal = double :terminal
+
+    expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a string").ordered
+    expect(terminal).to receive(:gets).and_return("TWIX").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(terminal).to receive(:gets).and_return("10").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX").ordered
+    
+    string_repeater = StringRepeater.new(terminal)
+    string_repeater.run
+  end
+end
