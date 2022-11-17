c9e651682614a736f9e2ea07f70516fae0df61d0
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-17 20:41:42 +0000

    Created interactive_calculator.rb and spec file

---
+++ mocking_bite_challenges/Terminal_IO/spec/interactive_calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'interactive_calculator'
+
+RSpec.describe InteractiveCalculator do
+  it 'subtracts two numbers using the terminal' do
+    terminal = double :terminal
+
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("4 - 3 = 1").ordered
+    
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    interactive_calculator.run
+  end
+end
